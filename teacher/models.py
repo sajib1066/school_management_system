6c6e1236fafc7e29450e8801a6f2e997bc3c4551
sajib1066 <sajib1066@gmail.com>
2019-07-12 09:25:58 +0600

    update model

--- teacher/models.py
+++ teacher/models.py
@@ -2,33 +2,33 @@ from django.db import models
 
 # Create your models here.
 class Designation(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
 class Department(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=true)
 
     def __str__(self):
         return self.name
 
 class District(models.Model):
-    name = models.CharField(max_length=45)
+    name = models.CharField(max_length=45, unique=True)
 
     def __str__(self):
         return self.name
 
 class Upazilla(models.Model):
     district = models.ForeignKey(District, on_delete=models.CASCADE)
-    name = models.CharField(max_length=45)
+    name = models.CharField(max_length=45, unique=true)
 
     def __str__(self):
         return self.name
 
 class Union(models.Model):
     upazilla = models.ForeignKey(Upazilla, on_delete=models.CASCADE)
-    name = models.CharField(max_length=45)
+    name = models.CharField(max_length=45, unique=True)
 
     def __str__(self):
         return self.name
