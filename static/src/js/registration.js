e11ee903df4fd45e23f0629ab2387f49d8104d52
sajib1066 <sajib1066@gmail.com>
2020-01-08 16:44:03 +0600

    add next and previous button in student registration form

--- static/src/js/registration.js
+++ static/src/js/registration.js
@@ -4,28 +4,64 @@ $('#academic_info_btn').on('click', function(){
   $('.personal_info').show();
 });
 
+// Presonal Information
 $('#personal_info_btn').on('click', function(){
   $('.personal_info').hide();
   $('.address_info').show();
 });
 
+$('#personal_info_prev_btn').on('click', function(){
+  $('.personal_info').hide();
+  $('.academic_info').show();
+});
+
+// Address Information
 $('#address_info_btn').on('click', function(){
   $('.address_info').hide();
   $('.guardian_info').show();
 });
 
+$('#address_info_prev_btn').on('click', function(){
+  $('.address_info').hide();
+  $('.personal_info').show();
+});
+
+// Guardian Information
 $('#guardian_info_btn').on('click', function(){
   $('.guardian_info').hide();
   $('.emergency_contact_info').show();
 });
 
+$('#guardian_info_prev_btn').on('click', function(){
+  $('.guardian_info').hide();
+  $('.address_info').show();
+});
+
+// Emergency Contact Information
 $('#emergency_contact_info_btn').on('click', function(){
   $('.emergency_contact_info').hide();
   $('.previous_academic_info').show();
 });
 
+$('#emergency_contact_info_prev_btn').on('click', function(){
+  $('.emergency_contact_info').hide();
+  $('.guardian_info').show();
+});
+
+// Previous Academic Information
 $('#previous_academic_info_btn').on('click', function(){
   $('.previous_academic_info').hide();
   $('.previous_academic_certificate').show();
   $('#submit').show();
+});
+
+$('#previous_academic_info_prev_btn').on('click', function(){
+  $('.previous_academic_info').hide();
+  $('.emergency_contact_info').show();
+});
+
+// Previous Academic Certificate Information
+$('#previous_academic_certificate_prev_btn').on('click', function(){
+  $('.previous_academic_certificate').hide();
+  $('.previous_academic_info').show();
 });
