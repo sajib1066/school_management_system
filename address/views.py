e2d891dbb621d2597630db7f9541cd8d91140ed4
sajib1066 <sajib1066@gmail.com>
2020-01-15 13:57:49 +0600

    update add union

--- address/views.py
+++ address/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import District, Upazilla, Union
 from .forms import DistrictForm, UpazillaForm, UnionForm
 
