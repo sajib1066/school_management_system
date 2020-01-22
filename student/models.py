3232a7b4a8084ce1b2c5b79863babb85d7bdc0c5
sajib1066 <sajib1066@gmail.com>
2020-01-22 17:17:25 +0600

    update StudentEnrolled model

--- student/models.py
+++ student/models.py
@@ -166,7 +166,7 @@ class AcademicInfo(models.Model):
 class EnrolledStudent(models.Model):
     class_name = models.ForeignKey(ClassInfo, on_delete=models.CASCADE)
     student = models.OneToOneField(AcademicInfo, on_delete=models.CASCADE)
-    roll = models.IntegerField(unique=True)
+    roll = models.IntegerField()
     date = models.DateField(auto_now_add=True)
 
     class Meta:
