cde02bf6cfd24716d3ad769a68ea77bb933e6e39
sajib1066 <sajib1066@gmail.com>
2020-01-24 18:18:01 +0600

    show information in dashboard

--- schoolmanagement/views.py
+++ schoolmanagement/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import student
+import teacher
+import employee
+import academic
 
 @login_required(login_url='login')
 def home_page(request):
-    return render(request, 'home.html')
+    total_student = student.models.PersonalInfo.objects.count()
+    total_teacher= teacher.models.PersonalInfo.objects.count()
+    total_employee = employee.models.PersonalInfo.objects.count()
+    total_class = academic.models.ClassRegistration.objects.count()
+    context = {
+        'student': total_student,
+        'teacher': total_teacher,
+        'employee': total_employee,
+        'total_class': total_class,
+    }
+    return render(request, 'home.html', context)
