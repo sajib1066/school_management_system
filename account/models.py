9928cb9fb4f909cf3e2b5eaf0c6128aa2bf49f13
sajib1066 <sajib1066@gmail.com>
2020-01-24 18:29:57 +0600

    create userprofile model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=45)
+    photo = models.ImageField(upload_to='admin/')
+    gender_select = (
+        ('male', 'Male'),
+        ('female', 'Female')
+    )
+    gender = models.CharField(choices=gender_select, max_length=6)
+    employee_select = (
+        ('admin', 'Admin'),
+        ('professor', 'Professor'),
+        ('teacher', 'Teacher'),
+        ('register', 'Register'),
+        ('student', 'Student'),
+    )
+    employee_type = models.CharField(choices=employee_select, max_length=15)
+
+    def __str__(self):
+        return self.name
