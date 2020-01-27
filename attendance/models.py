7abbbe2aa6176f6b669aa1f258d53c7c33fce14f
sajib1066 <sajib1066@gmail.com>
2020-01-27 12:22:50 +0600

    create student attendance model

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from academic.models import ClassRegistration
+from student.models import EnrolledStudent
 
-# Create your models here.
+class StudentAttendance(models.Model):
+    class_name = models.ForeignKey(ClassRegistration, on_delete=models.CASCADE)
+    student = models.OneToOneField(EnrolledStudent, on_delete=models.CASCADE)
+    status = models.IntegerField(default=0)
+    date = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['student', 'date']
+
+    def __str__(self):
+        return str(self.student)
