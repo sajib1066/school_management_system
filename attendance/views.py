70a5d8221318cbdf6cd171eb6694eaf3f2fb50a7
sajib1066 <sajib1066@gmail.com>
2020-01-29 18:51:04 +0600

    fix student attendance

--- attendance/views.py
+++ attendance/views.py
@@ -6,8 +6,8 @@ from academic.models import ClassRegistration
 def student_attendance(request):
     forms = SearchEnrolledStudentForm()
     class_name = request.GET.get('reg_class', None)
-    class_info = ClassRegistration.objects.get(id=class_name)
     if class_name:
+        class_info = ClassRegistration.objects.get(id=class_name)
         student = EnrolledStudent.objects.filter(class_name=class_name)
         context = {
             'forms': forms,
