00c6892f706904dc13a85223b918bc92bf52febf
sajib1066 <sajib1066@gmail.com>
2020-01-30 19:39:36 +0600

    update user profile models

--- account/models.py
+++ account/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=45)
-    photo = models.ImageField(upload_to='admin/')
+    name = models.CharField(max_length=45, blank=True, null=True)
+    photo = models.ImageField(upload_to='admin/', blank=True, null=True)
     gender_select = (
         ('male', 'Male'),
         ('female', 'Female')
     )
-    gender = models.CharField(choices=gender_select, max_length=6)
+    gender = models.CharField(choices=gender_select, max_length=6, blank=True, null=True)
     employee_select = (
         ('admin', 'Admin'),
         ('professor', 'Professor'),
@@ -17,7 +17,7 @@ class UserProfile(models.Model):
         ('register', 'Register'),
         ('student', 'Student'),
     )
-    employee_type = models.CharField(choices=employee_select, max_length=15)
+    employee_type = models.CharField(choices=employee_select, max_length=15, blank=True, null=True)
 
     def __str__(self):
         return self.name
