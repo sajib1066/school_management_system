860573ebdf615b40507290fdf84358eb8a7fc8eb
sajib1066 <sajib1066@gmail.com>
2020-04-19 19:12:32 +0600

    customize admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import StudentAttendance
 
-admin.site.register(StudentAttendance)
+class StudentAttendanceAdmin(admin.ModelAdmin):
+    list_display = ['class_name', 'student', 'date']
+
+admin.site.register(StudentAttendance, StudentAttendanceAdmin)
