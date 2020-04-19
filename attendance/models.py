0cf32e8551bb36d3dc0ca94cd14417ff8993b7e5
sajib1066 <sajib1066@gmail.com>
2020-04-19 19:11:42 +0600

    modify model unique fields

--- attendance/models.py
+++ attendance/models.py
@@ -2,14 +2,27 @@ from django.db import models
 from academic.models import ClassRegistration
 from student.models import EnrolledStudent
 
+class AttendanceManager(models.Manager):
+    def create_attendance(self, std_class, std_roll):
+        std_cls = ClassRegistration.objects.get(name=std_class)
+        std = EnrolledStudent.objects.get(roll=std_roll, class_name=std_cls)
+        std_att = StudentAttendance.objects.create(
+            class_name=std_cls,
+            student = std,
+            status = 1
+        )
+        return std_att
+
 class StudentAttendance(models.Model):
     class_name = models.ForeignKey(ClassRegistration, on_delete=models.CASCADE)
-    student = models.OneToOneField(EnrolledStudent, on_delete=models.CASCADE)
+    student = models.ForeignKey(EnrolledStudent, on_delete=models.CASCADE)
     status = models.IntegerField(default=0)
     date = models.DateField(auto_now_add=True)
+
+    objects = AttendanceManager()
 
     class Meta:
         unique_together = ['student', 'date']
 
     def __str__(self):
-        return str(self.student)
+        return str(self.student.student.personal_info.name)
