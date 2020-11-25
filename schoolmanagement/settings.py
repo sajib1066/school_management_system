3478926b23ea7abc0b6bb2c67e7d16fd1003e79a
lawalkeyd <lawalkeyd@gmail.com>
2020-11-25 17:00:09 +0100

    Initial commit

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -143,7 +143,7 @@ AUTH_USER_MODEL = 'account.Userss'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
