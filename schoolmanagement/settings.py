e3b297faac650d0225da5bcf94b7c2da53436f49
lawalkeyd <lawalkeyd@gmail.com>
2020-11-25 16:44:25 +0100

    Initial commit

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -134,6 +134,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'schoolmanagement.Userss'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
