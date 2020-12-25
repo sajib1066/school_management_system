9acf62308448ec06be7673e56679efdbbe778c76
lawalkeyd <lawalkeyd@gmail.com>
2020-12-25 23:06:45 +0100

    Roseville initial

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
